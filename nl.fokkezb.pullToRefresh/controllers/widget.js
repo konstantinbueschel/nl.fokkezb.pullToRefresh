0b2f53a393b4f753cfcebb18cb297225e91b8a6f
Konstantin Büschel <konstantin@die-interaktiven.de>
2016-12-01 16:25:41 +0100

    * updated code style

--- nl.fokkezb.pullToRefresh/controllers/widget.js
+++ nl.fokkezb.pullToRefresh/controllers/widget.js
@@ -1,107 +1,145 @@
-var refreshControl;
-var list;
+var LTAG = '[nl.fokkezb.pullToRefresh]',
+    refreshControl, list;
 
-$.refresh = refresh;
-
-$.show = show;
-$.hide = hide;
-$.getList = getList;
-$.getControl = getControl;
-
+/**
+ * SEF to organize otherwise inline code
+ *
+ * @private
+ * @param {Object} args
+ * @returns void
+ */
 (function constructor(args) {
-
-  if (args.dontInit || (!OS_IOS && !OS_ANDROID)){
-    if (!args.dontInit) console.warn('[pullToRefresh] only supports iOS and Android.');
-
-    if (_.isArray(args.children)) {
-      _.map(args.children, $.addTopLevelView);
+    
+    if (args.dontInit || (!OS_IOS && !OS_ANDROID)) {
+        
+        args.dontInit || Ti.API.warn(LTAG, 'Supports only iOS and Android.');
+        
+        _.isArray(args.children) && _.map(args.children, $.addTopLevelView);
+        
+        return;
     }
-
-    return;
-  }
-
-  if (!_.isArray(args.children) || !_.contains(['Titanium.UI.ListView', 'Titanium.UI.TableView', 'Ti.UI.ListView', 'Ti.UI.TableView', 'de.marcelpociot.CollectionView'], args.children[args.children.length-1].apiName)) {
-    console.error('[pullToRefresh] is missing required Ti.UI.ListView or Ti.UI.TableView or de.marcelpociot.CollectionView as first child element.');
-    return;
-  }
-
-  list = _.last(args.children);
-  delete args.children;
-
-  _.extend($, args);
-
-  if (OS_IOS) {
-    refreshControl = Ti.UI.createRefreshControl(args);
-    refreshControl.addEventListener('refreshstart', onRefreshstart);
-
-    list.refreshControl = refreshControl;
-
-    $.addTopLevelView(list);
-
-  } else if (OS_ANDROID) {
-    refreshControl = require('com.rkam.swiperefreshlayout').createSwipeRefresh(_.extend({
-      view: list
-    }, args));
-
-    refreshControl.addEventListener('refreshing', onRefreshstart);
-
-    $.addTopLevelView(refreshControl);
-  }
-
+    
+    if (!_.isArray(args.children) || !_.contains([
+            
+            'Titanium.UI.ListView', 'Titanium.UI.TableView', 'Ti.UI.ListView', 'Ti.UI.TableView', 'de.marcelpociot.CollectionView'
+        
+        ], args.children[args.children.length - 1].apiName)) {
+        
+        console.error(LTAG, 'Missing required Ti.UI.ListView or Ti.UI.TableView or de.marcelpociot.CollectionView as first child element.');
+        
+        return;
+    }
+    
+    list = _.last(args.children);
+    delete args.children;
+    
+    _.extend($, args);
+    
+    if (OS_IOS) {
+        
+        refreshControl = Ti.UI.createRefreshControl(args);
+        
+        refreshControl.addEventListener('refreshstart', onRefreshstart);
+        
+        list.refreshControl = refreshControl;
+        
+        $.addTopLevelView(list);
+    }
+    else if (OS_ANDROID) {
+        
+        refreshControl = require('com.rkam.swiperefreshlayout').createSwipeRefresh(_.extend({
+            
+            view: list
+            
+        }, args));
+        
+        refreshControl.addEventListener('refreshing', onRefreshstart);
+        
+        $.addTopLevelView(refreshControl);
+    }
+    
+    
+    // PUBLIC INTERFACE
+    $.refresh = refresh;
+    $.show = show;
+    $.hide = hide;
+    $.getList = getList;
+    $.getControl = getControl;
+    
 })(arguments[0] || {});
 
+
 function refresh() {
+    
+    if (!list) {
+        
+        return;
+    }
+    
+    show();
+    
+    onRefreshstart();
+    
+} // END refresh()
 
-  if (!list) {
-    return;
-  }
-
-  show();
-
-  onRefreshstart();
-}
 
 function hide() {
+    
+    if (!refreshControl) {
+        
+        return;
+    }
+    
+    if (OS_IOS) {
+        
+        refreshControl.endRefreshing();
+    }
+    else if (OS_ANDROID) {
+        
+        refreshControl.setRefreshing(false);
+    }
+    
+} // END hide()
 
-  if (!refreshControl) {
-    return;
-  }
-
-  if (OS_IOS) {
-    refreshControl.endRefreshing();
-
-  } else if (OS_ANDROID) {
-    refreshControl.setRefreshing(false);
-  }
-}
 
 function show() {
+    
+    if (!refreshControl) {
+        
+        return;
+    }
+    
+    if (OS_IOS) {
+        
+        refreshControl.beginRefreshing();
+    }
+    else if (OS_ANDROID) {
+        
+        refreshControl.setRefreshing(true);
+    }
+    
+} // END show()
 
-  if (!refreshControl) {
-    return;
-  }
-
-  if (OS_IOS) {
-    refreshControl.beginRefreshing();
-
-  } else if (OS_ANDROID) {
-    refreshControl.setRefreshing(true);
-  }
-}
 
 function getList() {
-  return list;
-}
+    
+    return list;
+    
+} // END getList()
+
 
 function getControl() {
-  return refreshControl;
-}
+    
+    return refreshControl;
+    
+} // END getControl()
+
 
 function onRefreshstart() {
-
-  $.trigger('release', {
-    source: $,
-    hide: hide
-  });
-
-}
+    
+    $.trigger('release', {
+        source: $,
+        hide: hide
+    });
+    
+} // END onRefreshstart()
